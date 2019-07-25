a623b9c70e544caf6e7b41acf0875e7fcbd00c5a
Lawrence Gober <goberlawrnce@icloud.com>
2019-07-25 17:47:35 -0400

    changed from hard game to easy game

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+
+    // Hide Content
+    $('.trivia-ques').hide();
+    $('.results').hide();
+
+    //Variables
+    let count = 90; //Seconds
+    let intervalId;
+    let correctAns = 0;
+    let wrongAns = 0;
+    let unanswered = 0;
+
+    // Functions
+
+    // Show Questions
+    function showQuestions() {
+        $('.trivia-ques').show();
+        $('#game-done').show();
+    }
+
+    // Timer
+    function countdownTimer() {
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    // Count Down
+    function decrement() {
+        count--;
+        $('#timer').text(" " + count + " " + "seconds");
+        if (number === 0) {
+            stop();
+            hide();
+            displaySummary();
+        }
+    }
+
+    // Clear Timer
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+    // Hide Questions
+    function hide() {
+        $('.trivia-ques').hide();
+        $('#game-done').hide();
+    }
+
+    // Summary
+    function displaySummary() {
+        $('.results').show();
+        unanswered = (4 - (correctAns + wrongAns));
+        $('#correctScore').text("Correct Answers: " + correctAns);
+        $('#wrongScore').text("Wrong Answers: " + wrongAns);
+        $('#unanswered').text("Unanswered: " + unanswered);
+    }
+
+    // CLICK EVENTS
+
+    // Start
+    $('#game-start').on('click', function() {
+        $('#game-start').hide();
+        showQuestions();
+        countdownTimer();
+    });
+
+    // Done
+    $('#game-done').on('click', function() {
+        $('#game-start').hide();
+        hide();
+        displaySummary();
+    });
+
+    // Radio Form Buttons
+    $('input[type=radio]').on('change', function() {
+        correctAns = $('input[value=correct]:checked').length;
+        wrongAns = $('input[value=wrong]:checked').length;
+        unanswered = (4 - (correctAns + wrongAns));
+    });
+
+});
